5b7f6db3c25ef173cc983ca7564706a0d892fcc5
edvella <git@edvella.com>
2014-02-02 20:28:01 +0100

    Beginner's Scraping With C# Part 1
    Tutorial available at
    http://www.edvella.com/Post/20/Beginners_Scraping_With_C_Part_1

---
+++ keywordCheck/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace keywordCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var url = "http://www.gametrailers.com";
+            var keywords = "final fantasy";
+            var pageContent = client.DownloadString(url);
+            var keywordLocation = pageContent.IndexOf(keywords, StringComparison.OrdinalIgnoreCase);
+            if (keywordLocation >= 0)
+            {
+                Console.WriteLine(url + " are talking about " + keywords + " today.");
+                Console.WriteLine("\nSnippet:\n" + pageContent.Substring(keywordLocation, 100));
+            }
+            Console.ReadKey();
+        }
+    }
+}
