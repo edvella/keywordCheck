0b6623db032bac7bf06ae4c9778f6d21864b2b05
edvella <git@edvella.com>
2014-02-28 20:52:27 +0100

    Beginner’s C# Part 2: Command Line Parameters
    http://www.edvella.com/Post/21/Beginners_C_Part_2_Command_Line_Parameters

--- keywordCheck/Program.cs
+++ keywordCheck/Program.cs
@@ -12,14 +12,19 @@ namespace keywordCheck
         static void Main(string[] args)
         {
             var client = new WebClient();
-            var url = "http://www.gametrailers.com";
-            var keywords = "final fantasy";
+            var url = args.Length > 0 && Uri.IsWellFormedUriString(args[0], UriKind.Absolute) ?
+                args[0] : "http://www.gametrailers.com";
+            var keywords = args.Length > 1 ? args[1] : "final fantasy";
             var pageContent = client.DownloadString(url);
             var keywordLocation = pageContent.IndexOf(keywords, StringComparison.OrdinalIgnoreCase);
             if (keywordLocation >= 0)
             {
                 Console.WriteLine(url + " are talking about " + keywords + " today.");
                 Console.WriteLine("\nSnippet:\n" + pageContent.Substring(keywordLocation, 100));
+            }
+            else
+            {
+                Console.WriteLine("Keyword not found!");
             }
             Console.ReadKey();
         }
